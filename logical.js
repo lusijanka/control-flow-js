9b2884a60bd05abef446c36707916a0a90819795
lusijanka <lucjamiodek@gmail.com>
2017-08-04 16:38:06 +0200

    js file modified

--- logical.js
+++ logical.js
@@ -12,18 +12,20 @@ if (result < 0) {
 };
 
 // function
-var a;
-var b;
 function value(a,b) {
 	var result = (a * a) + (2 * a * b) - (b * b);
 	if (result < 0) {
 	console.log('The result is below zero');
-} else if (result > 0) {
+	} else if (result > 0) {
 	console.log('The result is above zero');
-} else {
-	console.log('It seems the result is equal to 0!');
-};
-	return result;
+	} else {
+		if (isNaN(a) || isNaN(b)) == true {
+			console.log('You have not entered the valid number');
+		} else {
+			console.log('It seems the result is equal to 0!');
+		};
+	};
+return result;
 };
 
 console.log(value(5,4));
